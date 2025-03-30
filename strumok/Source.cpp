c60bcf9804b5c8bcd7d1b6cf708d4bec625bb282
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-03-30 23:02:10 +0300

    calculations are ready
    Except for S[16] frankly

--- strumok/Source.cpp
+++ strumok/Source.cpp
@@ -52,7 +52,7 @@ int main()
 	
 	unordered_map<string, int64_t> map;
 
-	for (int t = 0; t <= 20; t++)
+	for (int t = 0; t <= 31; t++)
 	{
 		int64_t result = cipher.Strm();
 
@@ -113,7 +113,7 @@ int main()
 
 
 	// Z_6 = FSM(S_21, R_1_6, R_2_6) ^ S_6; R_1_6 = R_2_5 + S_18 
-	S[21] = Z[6] ^ S[6] ^ R[6] - (R[5] + S[18]); 
+	S[21] = (Z[6] ^ S[6] ^ R[6]) - (R[5] + S[18]); 
 
 	R[8] = StrumokCipher::transform_T(R[6] + S[19]);
 
@@ -142,7 +142,7 @@ int main()
 
 
 	// Z_9 = FSM(S_24, R_1_9, R_2_9) ^ S_9; 
-	S[9] = Z[9] ^ StrumokCipher::FSM(S[24], R[8] + S[21], R[9]);
+	S[9] = Z[9] ^ StrumokCipher::FSM(S[24], R[8] + S[21], R[9]);   // S_21 is wrong
 	
 	// R_2_t+2 = T(R_2_t + S_13+t) 
 	R[13] = StrumokCipher::transform_T(R[11] + S[24]); 
@@ -151,7 +151,7 @@ int main()
 	S[25] = StrumokCipher::a_mul(S[9]) ^ StrumokCipher::ainv_mul(S[20]) ^ S[22];
 
 	// Z_10 = S_10 ^ FSM(S_25, R_1_10, R_2_10)
-	S[10] = Z[10] ^ StrumokCipher::FSM(S[25], R[9] + S[23], R[10]);
+	S[10] = Z[10] ^ StrumokCipher::FSM(S[25], R[9] + S[22], R[10]);
 
 	// use standart formula for calculating S_15 on the tact t
 	S[26] = StrumokCipher::a_mul(S[10]) ^ StrumokCipher::ainv_mul(S[21]) ^ S[23];
@@ -168,7 +168,7 @@ int main()
 	// We do not yet know the S_11, it can be found with Z_11, S_26 (they are known)
 	// Z_11 = FSM(S_26, R_1_11, R_2_11) ^ S_11; 
 	S[11] = Z[11] ^ StrumokCipher::FSM(S[26], R[10] + S[23], R[11]);
-	S[27] = StrumokCipher::ainv_mul(S[11]) ^ StrumokCipher::ainv_mul(S[22]) ^ S[24];
+	S[27] = StrumokCipher::a_mul(S[11]) ^ StrumokCipher::ainv_mul(S[22]) ^ S[24];
 
 	// Now we can find R again
 	R[16] = StrumokCipher::transform_T(R[14] + S[27]);
@@ -177,7 +177,7 @@ int main()
 	// We do not yet know the S_12, it can be found with Z_12, S_27 (they are known)
 	// Z_12 = FSM(S_27, R_1_12, R_2_12) ^ S_12; 
 	S[12] = Z[12] ^ StrumokCipher::FSM(S[27], R[11] + S[24], R[12]);
-	S[28] = StrumokCipher::ainv_mul(S[12]) ^ StrumokCipher::ainv_mul(S[23]) ^ S[25];
+	S[28] = StrumokCipher::a_mul(S[12]) ^ StrumokCipher::ainv_mul(S[23]) ^ S[25];
 
 	// Now we can find R again
 	R[17] = StrumokCipher::transform_T(R[15] + S[28]);
@@ -186,21 +186,22 @@ int main()
 	// We do not yet know the S_13, it can be found with Z_13, S_28 (they are known)
 	// Z_13 = FSM(S_28, R_1_13, R_2_13) ^ S_13;
 	S[13] = Z[13] ^ StrumokCipher::FSM(S[28], R[12] + S[25], R[13]);
-	S[29] = StrumokCipher::ainv_mul(S[13]) ^ StrumokCipher::ainv_mul(S[24]) ^ S[26];
+	S[29] = StrumokCipher::a_mul(S[13]) ^ StrumokCipher::ainv_mul(S[24]) ^ S[26];
 
-	// Also lets find S_14, S_15, S_16 
-	S[14] = Z[14] ^ StrumokCipher::FSM(S[28], R[13] + S[26], R[14]);
-	S[15] = Z[15] ^ StrumokCipher::FSM(S[29], R[14] + S[27], R[15]);
+	// Also lets find S_14, S_30, S_15, S_31, S_16 
+	S[14] = Z[14] ^ StrumokCipher::FSM(S[29], R[13] + S[26], R[14]);
+	S[30] = StrumokCipher::a_mul(S[14]) ^ StrumokCipher::ainv_mul(S[25]) ^ S[27];
 	
-	// lets find S_30 for S_16
-	S[30] = StrumokCipher::ainv_mul(S[14]) ^ StrumokCipher::ainv_mul(S[25]) ^ S[27];
-	S[16] = Z[16] ^ StrumokCipher::FSM(S[30], R[15] + S[28], R[16]); 
+	S[15] = Z[15] ^ StrumokCipher::FSM(S[30], R[14] + S[27], R[15]); 
+	
+	S[31] = StrumokCipher::a_mul(S[15]) ^ StrumokCipher::ainv_mul(S[26]) ^ S[28];
+	S[16] = Z[16] ^ StrumokCipher::FSM(S[31], R[15] + S[28], R[16]); 
 
 
 	// BINGO, we are done 
 	
 	// Verify S
-	for (int i = 10; i <= 30; i++)
+	for (int i = 6; i <= 31; i++)
 	{
 		if (map[format("S_{}", i)] == S[i])
 			cout << "S value " << std::dec << i << " guessed" << endl;
@@ -214,5 +215,8 @@ int main()
 		else
 			cout << "R value " << std::dec << i << " not guessed" << endl;
 	}
+
+	cout << map["S_16"] << endl;
+	cout << S[16];
 
  }
