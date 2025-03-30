db1fc101a25a231c1c3413cf9f6af321f9db0601
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-03-30 23:30:13 +0300

    add test module

--- strumok/Source.cpp
+++ strumok/Source.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
 #include <unordered_map>
 #include "strumok.h"
+#include "tests.hpp"
 
 
 int main()
 {
-	// ##########
-	// Tests 
-	// ##########
-	/*
-	vector<uint64_t> keyVec{ 0x8000000000000000, 0, 0, 0, 0, 0, 0, 0};
-	vector<uint64_t> IV{ 0x0000000000000004,0x0000000000000003,0x0000000000000002, 0x0000000000000001 };
 
-	StrumokCipher cipher(StrumokCipher::Mode::k512Bit); 
-	cipher.Init(keyVec, IV);
-
-	for (int index = 0; index < 7; index++)
+	Tester tester;
+	if (!tester.TestAll())
 	{
-		auto res = cipher.Strm();
-		cout << std::hex << res << endl;
-		cipher.Next();
+		cerr << "Tests failed";
+		return -1;
 	}
-	*/ 
 
 	vector<uint64_t> keyVec
 	{ 
